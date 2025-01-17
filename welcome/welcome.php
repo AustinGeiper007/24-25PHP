d9714546730e375f30aab0d9f42f2d78466a57ef
AustinG <austin.geisler@vtsd.com>
2025-01-17 09:04:08 -0500

    welcome.php now sanitizes the inputs in order to prevent malicious code

--- welcome/welcome.php
+++ welcome/welcome.php
@@ -1,8 +1,36 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php';
+
+// Setup Var and set empty
+$name = $email = "";
+
+// Condition to detect form data and sanitize/trim/clean it
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = sanitize_inputs($_POST["name"]);
+    $email = sanitize_inputs($_POST["email"]);
+}
+
+// Function to sanitize
+function sanitize_inputs($data){
+    //Trim data removes spaces at the beginning and end
+    //there are other characters included in "spaces,"
+    //ie tabs
+    $data = trim($data);
+
+    //Replaces slashes with the html counterpart, disabling it
+    $data = stripslashes($data);
+
+    //Makes the special characters in the html way, disabling
+    $data = htmlspecialchars($data);
+
+    // Allows the function to output the data
+    return $data;
+}
+
+?>
 
     <div class="row">
         <div class="col">
-            <p>Welcome <?php echo $_POST["name"] ?>, your email address is <?php echo $_POST["email"] ?>.</p>
+            <p>Welcome <?php echo $name; ?>, your email address is <?php echo $email; ?>.</p>
         </div>
     </div>
 
